54386be70a338fee483b5bc388456800d9e84207
Morten Hoffmann <morten.h.christensen@gmail.com>
2014-07-07 15:38:40 +0200

    login added

---
+++ KeisFiedlerShop/KeisFiedlerShop/Login.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="Login.aspx.cs" Inherits="KeisFiedlerShop.Login" %>
+
+<asp:Content runat="server" ContentPlaceHolderID="MainContent">
+    Brugernavn: <br />
+    <asp:TextBox runat="server" ID="usernameTextBox"  /><br />
+    Password: <br />
+    <asp:TextBox runat="server" ID="passwordTextBox" /><br />
+    <asp:Button runat="server" ID="loginBtn" OnClick="loginBtn_OnClick" Text="Log ind"/>
+</asp:Content>
---
+++ KeisFiedlerShop/KeisFiedlerShop/Login.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KeisFiedlerShop
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginBtn_OnClick(object sender, EventArgs e)
+        {
+            KeisFiedlerContainer keisFiedlerDb = new KeisFiedlerContainer();
+
+            var query = (from user in keisFiedlerDb.UserSet select user).ToList();
+
+            foreach (var user in query)
+            {
+                if (user.name == usernameTextBox.Text && user.password == SHA256.Create(passwordTextBox.Text).ToString())
+                {
+                    
+                }
+            }
+        }
+    }
+}
