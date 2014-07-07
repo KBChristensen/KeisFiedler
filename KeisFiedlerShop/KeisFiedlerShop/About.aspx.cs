c96ebab61caa101b0c0b4efbbcf7615c253dbba8
Morten Hoffmann <morten.h.christensen@gmail.com>
2014-07-07 13:00:09 +0200

    project added

---
+++ KeisFiedlerShop/KeisFiedlerShop/About.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KeisFiedlerShop
+{
+
+    public partial class About : Page
+    {
+        private string username;
+        private string password;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            KeisFiedlerContainer test = new KeisFiedlerContainer();
+
+            var query = (from variable in test.UserSet where variable.Id == 1 select variable).ToList();
+
+            foreach (var user in query)
+            {
+                label1.Text = user.name;
+                label2.Text = user.password;
+            }
+
+        }
+    }
+}
---
+++ KeisFiedlerShop/KeisFiedlerShop/test.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="test.aspx.cs" Inherits="KeisFiedlerShop.test" %>
+
+
+<asp:Content ContentPlaceHolderID="MainContent" runat="server">
+        <asp:FileUpload ID="FileUploadControl" runat="server"/>
+        <asp:Button ID="testBtn" OnClick="BtnClick" runat="server" Text="Button" />
+</asp:Content>    
+
+
+    
+    
