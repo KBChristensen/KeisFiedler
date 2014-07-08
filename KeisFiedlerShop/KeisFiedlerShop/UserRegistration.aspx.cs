0603e3738e82f44c250a945407e04f10d1397942
Morten Hoffmann <morten.h.christensen@gmail.com>
2014-07-08 21:18:00 +0200

    mkdkdk

--- KeisFiedlerShop/KeisFiedlerShop/UserRegistration.aspx.cs
+++ KeisFiedlerShop/KeisFiedlerShop/UserRegistration.aspx.cs
@@ -17,23 +17,23 @@ namespace KeisFiedlerShop
             
 		}
 
-	    protected void registerBtn_OnClick(object sender, EventArgs e)
-	    {
-	            string conString =
-	                "Data Source=Kasper-Pc\\SQLEXPRESS;Initial Catalog=KeisFiedler;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
-	            using (KeisFiedlerDbDataContext keisFiedlerDb = new KeisFiedlerDbDataContext(conString))
-	            {
-	                UserSet user = new UserSet();
-	                user.Username = usernameTextBox.Text;
-	                user.Name = nameTextBox.Text;
-	                user.Address = adressTextBox.Text;
-	                user.Zipcode = int.Parse(zipcodeTextBox.Text);
-	                user.Password = passwordTextBox.Text;
-	                user.Email = emailTextBox.Text;
-	                user.Userlevel = 3;
+        protected void registerBtn_OnClick(object sender, EventArgs e)
+        {
+            
+            string conString = "Data Source=MORTENLAPTOP\\SQLEXPRESS;Initial Catalog=KeisFiedler;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
+            using (KeisFiedlerDbDataContext keisFiedlerDb = new KeisFiedlerDbDataContext(conString))
+            {
+                UserSet user = new UserSet();
+                user.Username = usernameTextBox.Text;
+                user.Name = nameTextBox.Text;
+                user.Address = adressTextBox.Text;
+                user.Zipcode = int.Parse(zipcodeTextBox.Text);
+                user.Password = passwordTextBox.Text;
+                user.Email = emailTextBox.Text;
+                user.Userlevel = 3;
 
-	                keisFiedlerDb.UserSets.InsertOnSubmit(user);
-	                keisFiedlerDb.SubmitChanges();
+                keisFiedlerDb.UserSets.InsertOnSubmit(user);
+                keisFiedlerDb.SubmitChanges();
 	            }
 	        }
 	    }
